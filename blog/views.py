65e81dd8adddd4775906efafc01a8b55055b7243
Vanna Chan <vanna.th.chan@gmail.com>
2020-08-03 12:45:31 -0400

    Iterating through dummy data from views.py on template.

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+  {
+    'author': 'Vanna Chan',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 3, 2020'
+  },
+  {
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 4, 2020'
+  }
+]
+
 # this function will handle traffic from home
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   # return HttpResponse('<h1>Blog About</h1>')
