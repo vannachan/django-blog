d5f5a9b5ab1085ddf9878c7839a322eea104663a
Vanna Chan <vanna.th.chan@gmail.com>
2020-08-03 14:53:31 -0400

    Added Post model, got rid of dummy data, using real db data.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=100) # title a field of Post table
+  content = models.TextField()
+  date_posted = models.DateTimeField(default=timezone.now) # we are passing in the function but we are not executing the function, hence no timezone.now()
+  author = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted, delete the post too
+
+  def __str__(self):
+    return self.title
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post  # import Post class from models
+
 # Create your views here.
 
-posts = [
-  {
-    'author': 'Vanna Chan',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'August 3, 2020'
-  },
-  {
-    'author': 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 4, 2020'
-  }
-]
+# Dummy data:
+# posts = [
+#   {
+#     'author': 'Vanna Chan',
+#     'title': 'Blog Post 1',
+#     'content': 'First post content',
+#     'date_posted': 'August 3, 2020'
+#   },
+#   {
+#     'author': 'Jane Doe',
+#     'title': 'Blog Post 2',
+#     'content': 'Second post content',
+#     'date_posted': 'August 4, 2020'
+#   }
+# ]
 
 # this function will handle traffic from home
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all() # using ORM to grab all Posts from table
   }
   return render(request, 'blog/home.html', context)
 
