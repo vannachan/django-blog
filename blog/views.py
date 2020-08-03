8ab7f8c8a58286a20cf3b9df0bb995b92bc804ae
Vanna Chan <vanna.th.chan@gmail.com>
2020-08-03 12:54:27 -0400

    Added Template inheritance with base.html.

--- blog/views.py
+++ blog/views.py
@@ -26,5 +26,6 @@ def home(request):
   return render(request, 'blog/home.html', context)
 
 def about(request):
+
   # return HttpResponse('<h1>Blog About</h1>')
-  return render(request, 'blog/about.html')
+  return render(request, 'blog/about.html', {'title': 'About'})
