b926d415489f2ed0e0dff47f86d4364bc653c47a
Vanna Chan <vanna.th.chan@gmail.com>
2020-08-03 17:16:59 -0400

    Added login and logout capabilities.

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -17,9 +17,12 @@ from django.urls import path, include
 from django.contrib import admin
 
 from users import views as user_views # add alias for users view in case we import a lot of views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"), # specified arg because by default it looks in registration/login.html
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', include('blog.urls')),
 ]
