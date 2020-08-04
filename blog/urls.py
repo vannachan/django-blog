eefcef414fb5e55432ca56f30dc076e4fa7a2f8d
Vanna Chan <vanna.th.chan@gmail.com>
2020-08-04 16:08:27 -0400

    Added delete view.

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
-    PostUpdateView
+    PostUpdateView,
+    PostDeleteView
 )
 from . import views
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     # path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
