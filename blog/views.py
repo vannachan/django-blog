d53e789267c542ceb09d90084a943f338c293d62
Vanna Chan <vanna.th.chan@gmail.com>
2020-08-04 16:00:11 -0400

    Added update view and added author check.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
   ListView,
   DetailView,
-  CreateView
+  CreateView,
+  UpdateView
 )
 from django.http import HttpResponse
 
@@ -40,13 +42,27 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
   model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
   model = Post
   fields = ['title', 'content']
 
   def form_valid(self, form):
     form.instance.author = self.request.user
     return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    if self.request.user == post.author:
+      return True
+    return False
 
 def about(request):
   # return HttpResponse('<h1>Blog About</h1>')
